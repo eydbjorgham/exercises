1d60ff27c120350a2869ef4bcc323509723134f8
eydbjorgham <eyha0001@stud.kea.dk>
2025-09-11 14:19:48 +0200

    juhu animals

--- themeswitcher/index.js
+++ themeswitcher/index.js
@@ -3,7 +3,7 @@
 document.querySelector("select").addEventListener("change", changeColour);
 
 function changeColour() {
-  const valgtTema = document.querySelector("select").value;
-  document.querySelector("body").dataset.theme = valgtTema;
+  const valgtTema = document.querySelector("select").value; //Her siga vit virðið frá valdu select knappunum
+  document.querySelector("body").dataset.theme = valgtTema; //Her siga vit temaðið skal vera skift alt eftir hvat fyri virðið verður valt
   console.log(valgtTema);
 }
